94dcd56d82d67f9154b9f1c3944594a99bd76600
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-10-15 16:02:49 +0300

    Fix main page padding

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import YourStats from "./your_stats";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center flex-1 py-2">
-      <div className="flex flex-row w-screen max-w-[75rem] flex-wrap items-stretch gap-0 rounded-md shadow-md shadow-black overflow-hidden px-4">
+    <div className="flex flex-col items-center justify-center flex-1 w-screen px-4">
+      <div className="flex flex-row w-full max-w-[75rem] flex-wrap items-stretch gap-0 rounded-md shadow-md shadow-black overflow-hidden">
         <PlayWindow disabled />
         <YourStats />
       </div>
