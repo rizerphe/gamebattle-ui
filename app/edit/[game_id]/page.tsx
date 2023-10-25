dd9cb710ea4abbfcdefe86f338a93393a9d41419
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-10-25 14:13:34 +0300

    Add apostrophe escapes

--- app/edit/[game_id]/page.tsx
+++ app/edit/[game_id]/page.tsx
@@ -12,11 +12,14 @@ export default function EditorPage({
     <>
       <div className="bg-black p-3 rounded-xl bg-opacity-90 w-full flex flex-row items-center justify-between">
         <span>
-          You're editing another player's game. There is no version control.
-          This is a live environment.{" "}
+          You&apos;re editing another player&apos;s game. There is no version
+          control. This is a live environment.{" "}
           <span className="text-green-400">Be careful!</span>
         </span>
-        <span>Don't forget to build the game when you're done editing.</span>
+        <span>
+          Don&apos&apos;t forget to build the game when you&apos;re done
+          editing.
+        </span>
       </div>
       <EditorLayout api_route={api_route} game_id={game_id} />
     </>
