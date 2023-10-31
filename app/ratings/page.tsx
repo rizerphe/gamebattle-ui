9d36c61c626d18e979d3884e4f521155748e5d5a
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-10-31 16:15:01 +0200

    Add filters

--- app/ratings/page.tsx
+++ app/ratings/page.tsx
@@ -6,12 +6,6 @@ export default function Leaderboard() {
 
   return (
     <div className="bg-black bg-opacity-90 rounded-lg w-full overflow-hidden grid grid-cols-4">
-      <span className="text-2xl font-bold m-2 text-green-300">Author</span>
-      <span className="text-2xl font-bold m-2 text-green-300">Score</span>
-      <span className="text-2xl font-bold m-2 text-green-300">Reports</span>
-      <span className="text-2xl font-bold m-2 text-green-300">
-        Games played
-      </span>
       <Ratings api_route={api_route} />
     </div>
   );
--- app/ratings/table.tsx
+++ app/ratings/table.tsx
@@ -4,6 +4,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useState, useEffect } from "react";
 import { z } from "zod";
 import Link from "next/link";
+import { FaFilter } from "react-icons/fa";
 
 const RatingsSchema = z.array(
   z.tuple([
@@ -25,6 +26,8 @@ const RatingsSchema = z.array(
 export default function Ratings({ api_route }: { api_route: string }) {
   const [user] = useAuthState(auth);
   const [ratings, setRatings] = useState<z.infer<typeof RatingsSchema>>([]);
+  const [filterByReports, setFilterByReports] = useState(false);
+  const [filterByAccumulation, setFilterByAccumulation] = useState(false);
 
   useEffect(() => {
     const getLeaderboard = async () => {
@@ -41,46 +44,76 @@ export default function Ratings({ api_route }: { api_route: string }) {
     getLeaderboard();
   }, [user?.uid]);
 
-  return ratings.map(([metadata, position], index) => (
+  return (
     <>
-      <span
-        key={index * 4}
-        className="text-2xl font-bold p-2 flex flex-col border-t border-gray-500"
-      >
-        {metadata.author}
-        <span className="text-gray-400 text-sm flex flex-row">
-          {metadata.email}
-          &nbsp;&#x2022;&nbsp;
-          {metadata.name}
-        </span>
+      <span className="text-2xl font-bold m-2 text-green-300">Author</span>
+      <span className="text-2xl font-bold m-2 text-green-300">Score</span>
+      <span className="text-2xl font-bold m-2 text-green-300 flex flex-row items-center gap-2">
+        Reports
+        <FaFilter
+          onClick={() => setFilterByReports(!filterByReports)}
+          className="text-sm cursor-pointer text-gray-400 hover:text-gray-200"
+        />
       </span>
-      <span key={index * 4 + 1} className="text-xl font-bold p-4 border-t border-gray-500">
-        {position.elo.toFixed(0)} <span className="text-green-200">ELO</span> (
-        {position.place}&apos;th)
+      <span className="text-2xl font-bold m-2 text-green-300 flex flex-row items-center gap-2">
+        Games played
+        <FaFilter
+          onClick={() => setFilterByAccumulation(!filterByAccumulation)}
+          className="text-sm cursor-pointer text-gray-400 hover:text-gray-200"
+        />
       </span>
-      <Link
-        key={index * 4 + 2}
-        className={`text-xl font-bold p-4 border-t border-gray-500 ${
-          position.reports > 0 ? "hover:underline" : ""
-        } ${
-          position.reports > 5
-            ? position.reports > 25
-              ? "text-red-200"
-              : "text-yellow-200"
-            : "text-gray-200"
-        }`}
-        href={`/report/${metadata.email.split("@")[0].split(".")[0]}/1`}
-      >
-        {position.reports} Reports
-      </Link>
-      <span
-        key={index * 4 + 3}
-        className={`text-xl font-bold p-4 border-t border-gray-500 ${
-          position.accumulation < 5 ? "text-red-200" : "text-blue-200"
-        }`}
-      >
-        {position.accumulation.toFixed(0)} Games
-      </span>
+      {ratings
+        .filter(
+          ([metadata, position]) =>
+            (!filterByReports || position.reports > 5) &&
+            (!filterByAccumulation || position.accumulation < 5)
+        )
+        .map(([metadata, position], index) => (
+          <>
+            <span
+              key={index * 4}
+              className="text-2xl font-bold p-2 flex flex-col border-t border-gray-500"
+            >
+              {metadata.author}
+              <span className="text-gray-400 text-sm flex flex-row">
+                {metadata.email}
+                &nbsp;&#x2022;&nbsp;
+                {metadata.name}
+              </span>
+            </span>
+            <span
+              key={index * 4 + 1}
+              className="text-xl font-bold p-4 border-t border-gray-500"
+            >
+              {position.elo.toFixed(0)}{" "}
+              <span className="text-green-200">ELO</span> ({position.place}
+              &apos;th)
+            </span>
+            <Link
+              key={index * 4 + 2}
+              className={`text-xl font-bold p-4 border-t border-gray-500 ${
+                position.reports > 0 ? "hover:underline" : ""
+              } ${
+                position.reports > 5
+                  ? position.reports > 25
+                    ? "text-red-200"
+                    : "text-yellow-200"
+                  : "text-gray-200"
+              }`}
+              href={`/report/${metadata.email.split("@")[0].split(".")[0]}/1`}
+            >
+              {position.reports} Reports
+            </Link>
+            <span
+              key={index * 4 + 3}
+              className={`text-xl font-bold p-4 border-t border-gray-500 ${
+                position.accumulation < 5 ? "text-red-200" : "text-blue-200"
+              }`}
+            >
+              {position.accumulation.toFixed(0)} Games
+            </span>
+          </>
+        ))}
     </>
-  ));
+  );
 }
